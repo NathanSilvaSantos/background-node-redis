cbef8abd744fa9703e6d52556eb3cd8fc2448e13
NathanSilvaSantos <nathan013silva@gmail.com>
2020-10-03 23:42:03 -0300

    conectando a porta

--- src/server.js
+++ src/server.js
@@ -4,3 +4,7 @@ import express from 'express';
 const app = express();
 
 app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server started on port: ${process.env.PORT}`);
+});
